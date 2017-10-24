606ab668cd9ea0d955045569de09fb682ec0d704
Lucas Reis <lucasmreis@gmail.com>
2017-10-23 22:47:46 -0400

    trying pan responder

--- src/List/index.js
+++ src/List/index.js
@@ -5,7 +5,8 @@ import {
   Dimensions,
   Button,
   View,
-  Text
+  Text,
+  PanResponder
 } from "react-native";
 
 type Props = {
@@ -14,7 +15,42 @@ type Props = {
   removeSentenceAction: (sentence: string) => mixed
 };
 
-export default class extends React.Component<Props> {
+type State =
+  | {
+      status: "MOVING",
+      y: number
+    }
+  | { status: "IDLE" };
+
+export default class extends React.Component<Props, State> {
+  state = { status: "IDLE" };
+
+  panResponder = {};
+
+  componentWillMount() {
+    this.PanResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderGrant: this.onPanResponderGrant,
+      onPanResponderMove: this.onPanResponderMove,
+      onPanResponderRelease: this.onPanResponderRelease
+    });
+  }
+
+  onPanResponderGrant = (e, gestureState) => {
+    console.log("onPanResponderGrant");
+    this.setState({ status: "MOVING", dy: 0 });
+  };
+
+  onPanResponderMove = (e, gestureState) => {
+    console.log("onPanResponderMove");
+    this.setState({ status: "MOVING", dy: gestureState.dy });
+  };
+
+  onPanResponderRelease = () => {
+    console.log("onPanResponderRelease");
+    this.setState({ status: "IDLE" });
+  };
+
   getSize() {
     return {
       width: Dimensions.get("window").width,
@@ -38,20 +74,46 @@ export default class extends React.Component<Props> {
   render() {
     const { sentences } = this.props;
     return (
-      <ScrollView horizontal pagingEnabled style={styles.container}>
-        {sentences.map(this.renderPage)}
-      </ScrollView>
+      <View style={styles.container}>
+        <ScrollView horizontal pagingEnabled style={styles.scrollContainer}>
+          {sentences.map(this.renderPage)}
+        </ScrollView>
+        <View
+          style={[styles.action, { top: 0, left: 0, right: 0 }]}
+          {...this.panResponder.panHandlers}
+        >
+          <Text>ADD {JSON.stringify(this.state)}</Text>
+        </View>
+        <View
+          style={[styles.action, { bottom: 0, left: 0, right: 0 }]}
+          {...this.panResponder.panHandlers}
+        >
+          <Text>REMOVE {JSON.stringify(this.state)}</Text>
+        </View>
+      </View>
     );
   }
 }
+
+// {...this.panResponder.panHandlers}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+  scrollContainer: {
+    flex: 1
+  },
   page: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  action: {
+    position: "absolute",
+    height: 100,
+    backgroundColor: "#999",
     alignItems: "center",
     justifyContent: "center"
   }
